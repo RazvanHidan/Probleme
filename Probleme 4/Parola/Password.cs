83a65aa0404aa3b7bce81ae1949b5850b46b7589
RazvanHidan <razvan_hidan@yahoo.com>
2015-10-22 21:43:12 +0300

    Password

---
+++ Probleme 4/Parola/Password.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parola
+{
+    [TestClass]
+    public class Password
+    {
+        [TestMethod]
+        public void TestPassword()
+        {
+            Assert.AreEqual("random", PasswordGen (5, 3, 4, 2, 3));
+        }
+        void AddSymbols(ref string password, int elementAdd,string series)
+        {
+            var random = new Random();
+            for (int i = 0; i < elementAdd; i++)
+            {
+                password += series[random.Next(series.Length)];
+            }
+        }
+        void MixAll(ref string password, int length)
+        {
+            string mix = "";
+            AddSymbols(ref mix, length, password);
+            password = mix;
+        }
+        string PasswordGen(int length, int symbolsNr, int numbersNr, int lowercaseNr, int uppercaseNr)
+        {
+            const string symbols = "!@#$%^&*_+-=:?";
+            const string numbers = "23456789";
+            const string lowercase = "abcdefghijkmnpqrstuvxyz";
+            const string uppercase = "ABCDEFGHIJKMNPQRSTUVXYZ";
+            string password = "";
+            AddSymbols(ref password, symbolsNr, symbols);
+            AddSymbols(ref password, numbersNr, numbers);
+            AddSymbols(ref password, lowercaseNr, lowercase);
+            AddSymbols(ref password, uppercaseNr, uppercase);
+            MixAll(ref password, length);
+            return password;
+        }
+    }
+
+}
