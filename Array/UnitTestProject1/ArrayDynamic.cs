555cfaacb55700e2182d4606483f8257669ef795
RazvanHidan <razvan_hidan@yahoo.com>
2015-10-31 22:48:39 +0200

    Add new element

---
+++ Array/UnitTestProject1/ArrayDynamic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ArrayDynamic
+    {
+        [TestMethod]
+        public void AddNewMemberTest()
+        {
+            DynamicArray array = new DynamicArray();
+            array.Add(7);
+            Assert.AreEqual(array .LengthArray(), 1);
+            array.Add(1);
+            array.Add(5);
+            Assert.AreEqual(array.LengthArray(), 3);
+        }
+        public class DynamicArray
+        {
+            private int[] elements;
+            public DynamicArray()
+            {
+                elements = new int[0];
+            }
+            public int LengthArray()
+            {
+                return elements.Length;
+            }
+            public void Add(int newElement)
+            {
+                Array.Resize(ref elements, elements.Length + 1);
+                elements[elements.Length - 1] = newElement;
+            }
+        }
+    }
+}
