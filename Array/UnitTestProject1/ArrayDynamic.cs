d04954db897ec4fc54f17070332ab6b3dcef30a6
RazvanHidan <razvan_hidan@yahoo.com>
2015-11-01 01:13:16 +0200

    Insert new element

--- Array/UnitTestProject1/ArrayDynamic.cs
+++ Array/UnitTestProject1/ArrayDynamic.cs
@@ -7,7 +7,7 @@ namespace UnitTestProject1
     public class ArrayDynamic
     {
         [TestMethod]
-        public void AddNewMemberTest()
+        public void AddNewElement()
         {
             DynamicArray array = new DynamicArray();
             array.Add(7);
@@ -16,21 +16,54 @@ namespace UnitTestProject1
             array.Add(5);
             Assert.AreEqual(array.LengthArray(), 3);
         }
+        [TestMethod]
+        public void InsertNewElemennt()
+        {
+            DynamicArray array = new DynamicArray();
+            array.Add(7);
+            array.Insert(1, 1);
+            Assert.AreEqual(array.LengthArray(), 2);
+            array.Insert(5, 2);
+            Assert.AreEqual(array.LengthArray(), 3);
+            array.Insert(6, 2);
+            Assert.AreEqual(array.LengthArray(), 4);
+        }
+
         public class DynamicArray
         {
             private int[] elements;
+
             public DynamicArray()
             {
                 elements = new int[0];
             }
+
             public int LengthArray()
             {
                 return elements.Length;
             }
+
+            private void IncreaseLength(int lengthAdd)
+            {
+                Array.Resize(ref elements, elements.Length + lengthAdd);
+            }
+
             public void Add(int newElement)
             {
-                Array.Resize(ref elements, elements.Length + 1);
+                IncreaseLength(1);
                 elements[elements.Length - 1] = newElement;
+            }
+
+            public void Insert(int element, int position)
+            {
+                IncreaseLength(1);
+                int aux;
+                for (int i = position - 1; i < elements.Length; i++) 
+                {
+                    aux = elements[i];
+                    elements[i] = element;
+                    element = aux;
+                }
             }
         }
     }
