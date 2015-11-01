4ee303531064ce7a82d04a3131b7b780ea9d58fd
RazvanHidan <razvan_hidan@yahoo.com>
2015-11-01 13:23:35 +0200

    Get count

--- Array/UnitTestProject1/ArrayDynamic.cs
+++ Array/UnitTestProject1/ArrayDynamic.cs
@@ -11,10 +11,10 @@ namespace UnitTestProject1
         {
             DynamicArray array = new DynamicArray();
             array.Add(7);
-            Assert.AreEqual(array .LengthArray(), 1);
+            Assert.AreEqual(array .GetCount(), 1);
             array.Add(1);
             array.Add(5);
-            Assert.AreEqual(array.LengthArray(), 3);
+            Assert.AreEqual(array.GetCount(), 3);
         }
         [TestMethod]
         public void InsertNewElemennt()
@@ -22,11 +22,11 @@ namespace UnitTestProject1
             DynamicArray array = new DynamicArray();
             array.Add(7);
             array.Insert(1, 1);
-            Assert.AreEqual(array.LengthArray(), 2);
+            Assert.AreEqual(array.GetCount(), 2);
             array.Insert(5, 2);
-            Assert.AreEqual(array.LengthArray(), 3);
+            Assert.AreEqual(array.GetCount(), 3);
             array.Insert(6, 2);
-            Assert.AreEqual(array.LengthArray(), 4);
+            Assert.AreEqual(array.GetCount(), 4);
         }
         [TestMethod]
         public void DeleteElemennt()
@@ -34,21 +34,21 @@ namespace UnitTestProject1
             DynamicArray array = new DynamicArray();
             array.Add(7);
             array.Delete(1);
-            Assert.AreEqual(array.LengthArray(), 0);
+            Assert.AreEqual(array.GetCount(), 0);
             array.Add(1);
             array.Add(4);
             array.Delete(2);
-            Assert.AreEqual(array.LengthArray(), 1);
+            Assert.AreEqual(array.GetCount(), 1);
             array.Add(2);
             array.Add(8);
             array.Add(7);
             array.Delete(2);
-            Assert.AreEqual(array.LengthArray(), 3);
+            Assert.AreEqual(array.GetCount(), 3);
             array.Add(6);
             array.Add(1);
             array.Add(9);
             array.Delete(1);
-            Assert.AreEqual(array.LengthArray(), 5);
+            Assert.AreEqual(array.GetCount(), 5);
         }
         [TestMethod]
         public void GetValueOfPosition()
@@ -64,36 +64,65 @@ namespace UnitTestProject1
             Assert.AreEqual(array.GetElementAt(-1), 0);
             Assert.AreEqual(array.GetElementAt(12), 0);
         }
+        [TestMethod]
+        public void GetCount()
+        {
+            DynamicArray array = new DynamicArray();
+            Assert.AreEqual(array.GetCount(), 0);
+            array.Add(7);
+            Assert.AreEqual(array.GetCount(), 1);
+            array.Delete(1);
+            Assert.AreEqual(array.GetCount(), 0);
+            array.Add(4);
+            array.Add(2);
+            array.Add(8);
+            array.Add(7);
+            array.Add(4);
+            array.Add(2);
+            array.Add(8);
+            array.Add(7);
+            array.Add(7);
+            Assert.AreEqual(array.GetCount(), 9);
+        }
         public class DynamicArray
         {
             private int[] elements;
+            private int count;
 
             public DynamicArray()
             {
                 elements = new int[0];
+                count = 0;
             }
 
-            public int LengthArray()
+            public int GetCount()
             {
-                return elements.Length;
+                return count;
             }
 
             private void IncreaseLength(int lengthAdd)
             {
-                Array.Resize(ref elements, elements.Length + lengthAdd);
+                if (lengthAdd < 0)
+                    count += lengthAdd;
+                else
+                {
+                    while(elements.Length < count + lengthAdd)
+                        Array.Resize(ref elements, elements.Length + 8);
+                    count += lengthAdd;
+                }
             }
 
             public void Add(int newElement)
             {
                 IncreaseLength(1);
-                elements[elements.Length - 1] = newElement;
+                elements[count - 1] = newElement;
             }
 
             public void Insert(int element, int position)
             {
                 IncreaseLength(1);
                 int aux;
-                for (int i = position - 1; i < elements.Length; i++) 
+                for (int i = position - 1; i < count; i++) 
                 {
                     aux = elements[i];
                     elements[i] = element;
@@ -103,14 +132,15 @@ namespace UnitTestProject1
 
             public void Delete(int deletePosition)
             {
-                if (!((deletePosition == elements.Length) || (elements.Length == 1))) 
-                    for (int i = deletePosition-1; i < elements.Length - 1; i++)
+                if (!((deletePosition == count) || (count == 1)))
+                    for (int i = deletePosition - 1; i < count - 1; i++) 
                         elements[i] = elements[i + 1];
                 IncreaseLength(-1);
             }
+
             public int GetElementAt(int position)
             {
-                if ((position < elements.Length+1) && (position > 0)) 
+                if ((position < count + 1) && (position > 0)) 
                     return elements[position-1];
                 return 0;
             }
