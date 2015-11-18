7192cea692a2c79001a71937fa902b2482615f07
RazvanHidan <razvan_hidan@yahoo.com>
2015-11-17 23:33:12 -0800

    Library is emty

---
+++ HashTable/HashTable/HashTableTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Should;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTable
+{
+    [TestClass]
+    public class HashTableTest
+    {
+        [TestMethod]
+        public void LibraryIsEmty()
+        {
+            var diverta = new Library();
+            diverta.keys.ShouldBeNull();
+        }
+    }
+}
---
+++ HashTable/HashTable/Library.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTable
+{
+    public class Library
+    {
+        private string book;
+        public string[] keys;
+        public string[] buckets;
+        private int count;
+        public Library()
+        {
+            keys = null;
+            buckets = null;
+            count = 0;
+        }
+
+        public void Add(string value)
+        {
+            if (count == 0)
+                GenerateArray();
+        }
+
+        private void GenerateArray()
+        {
+            keys = new Array[25];
+            buckets = new Array[25];
+        }
+    }
+}
