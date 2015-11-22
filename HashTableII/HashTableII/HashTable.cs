c2f614cf24369c7872c04f16a78c03163c45b895
RazvanHidan <razvan_hidan@yahoo.com>
2015-11-22 07:09:59 -0800

    Hashtable is emty and initialization capacity

---
+++ HashTableII/HashTableII/HashTable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableII
+{
+    public interface IHasher<in T>
+    {
+        int GetHashCode(T obj);
+    }
+
+    public struct Bucket<key, value>
+    {
+        key keyValue;
+        value infoKey;
+        int next
+        {
+            get
+            {
+                return -1;
+            }
+        }
+    } 
+
+    public class Hashtable<key, value>:IHasher<key>
+    {
+        private key keyValue;
+        private int[] keys;
+        private Bucket<key, value>[] buckets;
+        private int count;
+
+        public Hashtable()
+        {
+            keys = null;
+            buckets = null;
+            count = 0;
+        }
+
+        public Hashtable(int capacity)
+        {
+            Array.Resize(ref keys, capacity);
+            Array.Resize(ref buckets, capacity);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        private int GetHashCode(key obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IHasher<key>.GetHashCode(key obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HashTableII/HashTableII/HashTableIITest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace HashTableII
+{
+    [TestClass]
+    public class HashTableIITest
+    {
+        [TestMethod]
+        public void HashtableIsEmty()
+        {
+            Hashtable<string, int> phonebook = new Hashtable<string, int>();
+            phonebook.Count.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void Size_Initialization_Hashtable()
+        {
+            Hashtable<string, int> phonebook = new Hashtable<string, int>(10);
+            phonebook.Count.ShouldEqual(0);
+        }
+    }
+}
